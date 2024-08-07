4f0000b667d9518d42545ed0a8326074ad313bfa
oddymagoo <38410255+oddymagoo@users.noreply.github.com>
2024-08-07 10:47:01 +1000

    fixed errors

--- modules/Core/variables.tf
+++ modules/Core/variables.tf
@@ -18,7 +18,7 @@ variable "ami_filter" {
 }
 
 variable "environment" {
-    description = "Deployment environment
+    description = "Deployment environment"
 
     type               = object ({
         name           = string
