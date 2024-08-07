0368a15b8a21b3b6bfaf0cbc66009ec85d6c320e
oddymagoo <38410255+oddymagoo@users.noreply.github.com>
2024-08-07 10:55:55 +1000

    Removed version from providers and main

--- modules/Core/main.tf
+++ modules/Core/main.tf
@@ -31,7 +31,7 @@ module "main_vpc" {
 
 module "main_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  #version = "4.13.0"
 
   name                = "main-security-group"
   description         = "Security Group for Main service"
@@ -45,7 +45,7 @@ module "main_sg" {
 
 module "main_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+  #version = "~> 6.0"
 
   name               = "main-alb"
   load_balancer_type = "application"
@@ -76,7 +76,7 @@ module "main_alb" {
 
 module "main_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+  #version = "6.5.2"
   
   name = "main-asg"
   min_size                  = var.environment.asg_min
